1fb92af824063a3619769a2f26c3f771892d798f
liaojiexin <570839281@qq.com>
2021-02-19 16:12:05 +0800

    第一个netty程序

---
+++ src/main/java/com/liaojiexin/netty/handler/EchoServerHandler.java
@@ -0,0 +1,24 @@
+package com.liaojiexin.netty.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @ClassName: EchoServerHandler
+ * @Description: TODO
+ * @version: 1.0
+ * @author: liaojiexin
+ * @date: 2021/2/19 16:10
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg); // (1)将接收到的数据响应给客服端
+        ctx.flush(); // (2)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+}
