92597bf17681d9e646a97c95d2a270060a581a09
liaojiexin <570839281@qq.com>
2021-03-01 22:19:46 +0800

    commit

--- chapter4-server/src/main/java/com/liaojiexin/netty/handler/ServerHandler.java
+++ chapter4-server/src/main/java/com/liaojiexin/netty/handler/ServerHandler.java
@@ -1,4 +1,40 @@
 package com.liaojiexin.netty.handler;
 
-public class ServerHandler {
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.CharsetUtil;
+
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in=(ByteBuf) msg;
+        System.out.println("Server received:"+in.toString(CharsetUtil.UTF_8));  //将消息记录到控制台
+        ctx.write(in);  //将接收到的消息写给发送者，而不刷新出站信息
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = new NioServerSocketChannel();
+        ByteBuf buf =Unpooled.copiedBuffer("your data",CharsetUtil.UTF_8);
+        ChannelFuture cf =channel.writeAndFlush(buf);
+        cf.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                if (channelFuture.isSuccess()) {
+                    System.out.println("Write successful");
+                } else {
+                    System.err.println("Write error");
+                    channelFuture.cause().printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause){
+        cause.printStackTrace();    //打印异常栈跟踪
+        ctx.close();    //关闭该Channel
+    }
 }
--- chapter4-server/src/main/java/com/liaojiexin/netty/server/Server.java
+++ chapter4-server/src/main/java/com/liaojiexin/netty/server/Server.java
@@ -1,4 +1,54 @@
 package com.liaojiexin.netty.server;
 
-public class Server {
+import com.liaojiexin.netty.handler.ServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+public class Server extends ChannelInboundHandlerAdapter {
+    private final int port;
+
+    public Server(int port){
+        this.port=port;
+    }
+
+    public void start() throws InterruptedException {
+        EventLoopGroup boss =new NioEventLoopGroup();
+        EventLoopGroup worker=new NioEventLoopGroup();
+        try{
+            ServerBootstrap bootstrap=new ServerBootstrap();
+            bootstrap.group(boss,worker)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new ServerHandler());
+                        }
+                    });
+            //6.异步绑定服务器，调用sync方法阻塞等待知道绑定完成
+            ChannelFuture channelFuture=bootstrap.bind().sync();
+            //7.获取Channel的CloseFuture，并且阻塞当前线程知道它完成
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            boss.shutdownGracefully().sync();
+            worker.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        //设置端口值（如果端口参数格式不正确，则抛出一个NumberFormatException）
+        if (args.length!=1){
+            System.err.println("Usage:"+Server.class.getSimpleName()+"<port>");
+        }
+        int port=Integer.parseInt(args[0]);
+        new Server(port).start();       //调用服务器start方法
+    }
 }
