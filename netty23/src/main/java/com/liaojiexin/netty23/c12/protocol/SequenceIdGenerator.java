e38a2fc0c293edac2cd73d919110bca1a40dd327
liaojiexin <570839281@qq.com>
2023-01-24 21:06:12 +0800

    Netty

---
+++ netty23/src/main/java/com/liaojiexin/netty23/c12/protocol/SequenceIdGenerator.java
@@ -0,0 +1,11 @@
+package com.liaojiexin.netty23.c12.protocol;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public abstract class SequenceIdGenerator {
+    private static final AtomicInteger id = new AtomicInteger();
+
+    public static int nextId() {
+        return id.incrementAndGet();
+    }
+}
